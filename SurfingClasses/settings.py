4085a2710246164a8b86532d3d2be4cfe549499e
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-11-24 22:56:47 +0000

    extras to app

--- SurfingClasses/settings.py
+++ SurfingClasses/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -19,6 +18,7 @@ if os.path.isfile('env.py'):
     
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 if os.path.exists('env.py'):
 
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['surfingbookingsystem.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'SurfingClasses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
