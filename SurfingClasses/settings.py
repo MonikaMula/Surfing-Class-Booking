a3bb28c237123cbb65050f0f9883ef81a8648c8c
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-11-25 16:25:30 +0000

    correcting

--- SurfingClasses/settings.py
+++ SurfingClasses/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+# import os
 from pathlib import Path
 import os
 import dj_database_url
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['surfingbookingsystem.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['surfingbookingsystem.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
