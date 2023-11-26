f0709778ad9a8f9b1f11b8002c155ba63e0b1970
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-11-26 15:33:50 +0000

    models_for_SurfingClass

--- BookingSystem/models.py
+++ BookingSystem/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class SurfingClass(model.Model):
+    class_name = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    location = models.CharField(max_length=200)
+    max_capacity = models.IntegerField()
+    current_capacity = models.IntegerField(default=0)
+    
+    def available_spots(self):
+        return self.max_capacity - self.current_capacity
