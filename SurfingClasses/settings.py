b3f7d9ab405843dfca748dd3e2b16830a3e1ba51
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-11-26 23:19:52 +0000

    debug

--- SurfingClasses/settings.py
+++ SurfingClasses/settings.py
@@ -31,7 +31,7 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['surfingclasses.herokuapp.com', 'localhost', '127.0.0.1']
 
