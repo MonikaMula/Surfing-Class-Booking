904d5c5aef8ad395b28887f1dc8ed5557d67847e
@monika.sw <114880451+MonikaMula@users.noreply.github.com>
2023-12-02 16:41:43 +0000

    new template-status.html and tab

--- BookingSystem/views.py
+++ BookingSystem/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'bookingsystem/dashboard.html')
+    return render(request, 'bookingsystem/home.html')
 
 def classes(request):
     return render(request, 'bookingsystem/classes.html')
